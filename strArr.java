a3b92b8637c3698bb6bb5521270a72306071ab47
yangersss <edwardyang609@gmail.com>
2022-10-14 11:58:11 -0400

    HELP GH IWHIHUFEIWHEF

--- strArr.java
+++ strArr.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class strArr {
     public static void main (String[] args) throws Exception{
         File myFile = new File ("./Top_40s_September_22.txt");
+        String [] arr = new String[0];
 
         try{
             Scanner myScanner = new Scanner(myFile);
@@ -13,7 +14,7 @@ public class strArr {
                 myScanner.nextLine();
             }
             
-            String[] arr = new String[c];
+            arr = new String[c];
             Scanner myScanner2 = new Scanner(myFile);
             int i = 0;
             while(myScanner2.hasNext()){
@@ -27,6 +28,50 @@ public class strArr {
         }
         catch(Exception e){
             e.printStackTrace();
+        }
+
+        // time to sort the songs and artists into a separate array
+        String[] songs = new String[arr.length / 2];
+        String[] artists = new String[arr.length / 2];
+
+        for (int i = 0; i < arr.length; i++){
+            if (i % 2 == 0){
+                songs[i / 2] = arr[i];
+            }
+            else{
+                artists[i / 2] = arr[i];
+            }
+        }
+
+        System.out.println("\n\n\n\n\n");
+
+        for (String s : songs){
+            System.out.println(s);
+        }
+        System.out.println("\n\n\n");
+        for (String s : artists){
+            System.out.println(s);
+        }
+
+
+        // time to sort the arrays yayyy kappa
+        int current_lowest;
+
+        for (int i = 0; i < songs.length; i++){
+            current_lowest = i;
+            for (int j = i + 1; j < songs.length; j++){
+                if (songs[current_lowest].compareTo(songs[j]) >= 1){
+                    current_lowest = j;
+                }
+            }
+            String temp = songs[i];
+            songs[i] = songs[current_lowest];
+            songs[current_lowest] = temp;
+        }
+
+        System.out.println("\n\n\n SORTED ARRAY TIME YAYY \n\n\n");
+        for (String s : songs){
+            System.out.println(s);
         }
     }
 }
